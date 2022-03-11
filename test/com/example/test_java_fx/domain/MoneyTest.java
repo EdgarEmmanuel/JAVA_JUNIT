cecc4243476a5a9f8a1f696ce7cf7b619db950d2
Edgar <mmnledgar@gmail.com>
2022-03-11 13:01:00 +0000

    making the unit test for suming two money

--- test/com/example/test_java_fx/domain/MoneyTest.java
+++ test/com/example/test_java_fx/domain/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoneyTest {
 
     @Test
-    public void test()
+    public void sum_of_two_money_produce_exact_result()
     {
         Money money = new Money(1,2,3,4,5,6);
         Money money_two = new Money(1,2,3,4,5,6);
@@ -15,5 +15,10 @@ class MoneyTest {
         Money sum = Money.operatorPlus(money, money_two);
 
         assertEquals(2, sum.oneCentCount);
+        assertEquals(4, sum.tenCentCount);
+        assertEquals(6, sum.quarterCount);
+        assertEquals(8, sum.oneDollarCount);
+        assertEquals(10, sum.fiveDollarCount);
+        assertEquals(12, sum.twentyDollarCount);
     }
 }
