27efe3ff2034ed4a586a038e3938b353458b5cbe
Edgar <mmnledgar@gmail.com>
2022-03-11 10:50:00 +0000

    setting up the project for unit testing

---
+++ src/main/java/com/example/test_java_fx/tests/MoneyTest.java
@@ -0,0 +1,10 @@
+package com.example.test_java_fx.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class MoneyTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.test_java_fx {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.junit.jupiter.api;
 
     opens com.example.test_java_fx to javafx.fxml;
     exports com.example.test_java_fx;
