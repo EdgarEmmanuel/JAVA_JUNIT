fb273f4e5df0ed4ac1eb8582b6bfb533b16ad174
Edgar <mmnledgar@gmail.com>
2022-03-17 17:29:28 +0000

    feature-add-house: make the ui

--- src/main/java/com/example/test_java_fx/HelloController.java
+++ src/main/java/com/example/test_java_fx/HelloController.java
@@ -1,10 +1,17 @@
 package com.example.test_java_fx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class HelloController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Button button_oneCent;
 
@@ -26,4 +33,14 @@ public class HelloController {
     @FXML
     private Button button_returnMoney;
 
+    @FXML
+    private ImageView imageView_oneCent;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        File file = new File("src/main/resources/images/oneCent.png");
+        Image image = new Image(file.toURI().toString());
+        imageView_oneCent.setImage(image);
+    }
 }
