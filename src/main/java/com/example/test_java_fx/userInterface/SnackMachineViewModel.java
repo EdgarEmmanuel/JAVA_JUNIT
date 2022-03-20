4ae2c2229d2e773ee0a26bcc1690bbd585c24a65
Edgar <mmnledgar@gmail.com>
2022-03-20 15:37:27 +0000

    feature-add-house: woriking on the viewModel

---
+++ src/main/java/com/example/test_java_fx/userInterface/SnackMachineViewModel.java
@@ -0,0 +1,13 @@
+package com.example.test_java_fx.userInterface;
+
+import com.example.test_java_fx.domain.SnackMachine;
+
+public class SnackMachineViewModel {
+    private SnackMachine snackMachine;
+
+
+    public SnackMachineViewModel(SnackMachine snackMachine)
+    {
+        this.snackMachine = snackMachine;
+    }
+}
